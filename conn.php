9d3fd284560cf275fe2f77207c7b09cdd76f2519
Rizki-Setiawan <17.rizkisetiawan@gmail.com>
2021-06-17 14:24:49 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,5 @@
 	$conn = mysqli_connect("localhost","root","","187006101_tugasws");
 	if (mysqli_connect_errno()){
 		echo "Koneksi database gagal : " . mysqli_connect_error();
-	} else{
-		echo "Koneksi database berhasil";
-	}
+	} 
 ?>
